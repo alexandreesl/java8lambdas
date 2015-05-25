005c53a474392a89fe2254ebcb9c2c55b2e9516e
alexandreesl <alexandreesl@gmail.com>
2015-05-24 23:39:12 -0300

    commit of first lambda example

---
+++ src/main/java/com/alexandreesl/handson/LambdaBaseExample.java
@@ -0,0 +1,48 @@
+package com.alexandreesl.handson;
+
+public class LambdaBaseExample {
+
+	public static void main(String[] args) {
+
+		// pre-java 8, with inner classes
+
+		for (int i = 1; i <= 10; i++) {
+
+			final int number = i;
+
+			Thread thread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+
+					System.out.println("The number is " + number);
+
+				}
+
+			});
+
+			thread.start();
+
+		}
+
+		// java 8 version with lambdas
+
+		for (int i = 1; i <= 10; i++) {
+
+			final int number = i;
+
+			Runnable runnable = () -> System.out
+					.println("The number with a lambda is " + number);
+
+			Thread thread = new Thread(runnable);
+
+			thread.start();
+
+			System.out.println("The 'class' of our lambda: "
+					+ runnable.getClass());
+
+		}
+
+	}
+
+}
